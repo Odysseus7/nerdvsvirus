a82d3e0c13f71d9d45706a03850545b0f6cca48d
s1128571 <s1128571@student.hsleiden.nl>
2021-04-06 11:39:31 +0200

    HOi dit is een test

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,39 @@
-public class Game {
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+import java.util.Map;
+
+public class Game extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+        gameSettings.setWidth(1200);
+        gameSettings.setHeight(800);
+        gameSettings.setTitle("Nerds VS Virus");
+    }
+
+    @Override
+    protected void initGame(){
+
+    }
+
+    @Override
+    protected void initInput(){
+
+
+    }
+
+    @Override
+    protected void initUI() {
+
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars){
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
